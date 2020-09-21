3d49f7c8921d0a150525695111413515f9052948
anwar03 <anwarazim03@gmail.com>
2020-09-21 23:02:25 +0600

    Create BookCheckoutTest.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::patch('/books/{book}-{slug}', 'BooksController@update');
 Route::delete('/books/{book}-{slug}', 'BooksController@destroy');
 
 Route::post('/author', 'AuthorController@store');
+
+
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
